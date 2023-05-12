dea00adf45ef26779acc45a8df8f5737aebc5b04
GreyKL <GreyWuhu@gmail.com>
2023-05-12 13:49:40 +0800

    1.两数相加递归解法

--- src/com/kl/medium/AddTwoNumbers.java
+++ src/com/kl/medium/AddTwoNumbers.java
@@ -12,6 +12,14 @@ import java.util.List;
 public class AddTwoNumbers {
 
     public static void main(String[] args) {
+        /*
+        给你两个 非空 的链表，表示两个非负的整数。它们每位数字都是按照 逆序 的方式存储的，并且每个节点只能存储 一位 数字。
+         请你将两个数相加，并以相同形式返回一个表示和的链表。
+        你可以假设除了数字 0 之外，这两个数都不会以 0 开头。
+        输入：l1 = [2,4,3], l2 = [5,6,4]
+        输出：[7,0,8]
+        解释：342 + 465 = 807.
+         */
         ListNode n1 = new ListNode(3,null);
         ListNode n2 = new ListNode(4,n1);
         ListNode n3 = new ListNode(2,n2);
@@ -138,6 +146,30 @@ public class AddTwoNumbers {
   }
 
 
+    public ListNode addTwoNumbersNew(ListNode l1, ListNode l2) {
+        return add(l1, l2, 0);
+    }
+
+    /**
+     返回两个链表相加的头部
+     */
+    public ListNode add(ListNode l1, ListNode l2, int bit) {
+        if (l1 == null && l2 == null && bit == 0) {
+            return null;
+        }
+        int val = bit;
+        if (l1 != null) {
+            val += l1.val;
+            l1 = l1.next;
+        }
+        if (l2 != null) {
+            val += l2.val;
+            l2 = l2.next;
+        }
+        ListNode node = new ListNode(val % 10);
+        node.next = add(l1, l2, val / 10);
+        return node;
+    }
 
     public static class ListNode {
         int val;
---
+++ src/com/kl/medium/LengthOfLongestSubstring.java
@@ -0,0 +1,25 @@
+package com.kl.medium;
+
+/**
+ * 无重复字符的最长子串
+ * @author: kl
+ * @date: 2023/5/12  10:28
+ */
+public class LengthOfLongestSubstring {
+
+    public static void main(String[] args) {
+        /*
+        给定一个字符串 s ，请你找出其中不含有重复字符的 最长子串 的长度。
+        输入: s = "abcabcbb"
+        输出: 3
+        解释: 因为无重复字符的最长子串是 "abc"，所以其长度为 3。
+         */
+        String s ="abcabcbb";
+        System.out.println(lengthOfLongestSubstring(s));
+    }
+
+    public static int lengthOfLongestSubstring(String s) {
+
+        return 0;
+    }
+}
